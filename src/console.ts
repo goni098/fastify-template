4694ac4180ffb68e7f7bf0154188282238daca80
goni098 <mtheanh2910@gmail.com>
2025-03-22 10:42:47 +0700

    scan console

--- src/console.ts
+++ src/console.ts
@@ -1,21 +1,17 @@
-import {
-	EventId,
-	type PaginatedEvents,
-	SuiClient,
-	getFullnodeUrl
-} from "@mysten/sui/client"
-import { Duration, Effect as E, Effect, flow, Option as O, pipe } from "effect"
-import { Chunk as C, Array as A } from "effect"
+import { SuiClient, getFullnodeUrl } from "@mysten/sui/client"
+import { Duration, Effect as E, Effect, Option as O, flow, pipe } from "effect"
+import { Array as A, Boolean as B, Chunk as C } from "effect"
+import type { NoSuchElementException } from "effect/Cause"
 import { establishConnection } from "./database/db.js"
 import { EventRepository } from "./database/repositories/event.repository.js"
-import { SettingRepository } from "./database/repositories/setting.repository.js"
+import {
+	type EventCursor,
+	SettingRepository
+} from "./database/repositories/setting.repository.js"
 import type { DatabaseException } from "./exceptions/database.ex.js"
+import type { SuiClientException } from "./exceptions/sui-client.ex.js"
 import { Web3Client } from "./shared/sui.js"
 import type { Result } from "./types/result.type.js"
-import { SuiClientException } from "./exceptions/sui-client.ex.js"
-import { NoSuchElementException } from "effect/Cause"
-
-type Cursor = EventId
 
 const MODULE = "vending_machine"
 
@@ -33,16 +29,22 @@ function main() {
 		E.let("db", () => establishConnection()),
 		E.let("eventRepository", ({ db }) => new EventRepository(db)),
 		E.let("settingRepository", ({ db }) => new SettingRepository(db)),
-		E.bind("cursor", ({ settingRepository }) =>
-			settingRepository.get("lastest_event_seq").pipe(
-				E.zip(settingRepository.get("lastest_event_tx_didest")),
-				E.map(([eventSeq, txDigest]) => ({ eventSeq, txDigest }))
+		E.bind("cursor", ({ settingRepository, client }) =>
+			settingRepository.getLastestCursor().pipe(
+				E.flatten,
+				E.catchTag("NoSuchElementException", () => getDefaultCursor(client)),
+				E.tap(cursor =>
+					settingRepository.set("lastest_event_seq", cursor.eventSeq)
+				),
+				E.tap(cursor =>
+					settingRepository.set("lastest_event_tx_didest", cursor.txDigest)
+				)
 			)
 		),
-		E.flatMap(({ client, eventRepository, settingRepository }) =>
-			E.iterate(O.none<Cursor>(), {
+		E.flatMap(({ client, eventRepository, settingRepository, cursor }) =>
+			E.iterate(O.some(cursor), {
 				body: cursor =>
-					handleEvents(client, cursor, eventRepository, settingRepository),
+					scan(client, cursor, eventRepository, settingRepository),
 				while: () => true
 			})
 		),
@@ -52,40 +54,81 @@ function main() {
 
 main()
 
-// function handleEvents(
-// 	client: Web3Client,
-// 	cursor: Cursor,
-// 	eventRepository: EventRepository,
-// 	settingRepository: SettingRepository
-// ): Result<O.Option<Cursor>, DatabaseException> {
-// 	return pipe(
-// 		cursor,
+function scan(
+	client: Web3Client,
+	cursor: O.Option<EventCursor>,
+	eventRepository: EventRepository,
+	settingRepository: SettingRepository
+): Result<O.Option<EventCursor>, DatabaseException | SuiClientException> {
+	return pipe(
+		cursor,
+		O.match({
+			onNone: flow(
+				O.none,
+				E.succeed,
+				E.tap(() => console.log("scanned to latest")),
+				E.tap(() => E.sleep(Duration.seconds(10)))
+			),
+			onSome: cursor =>
+				handleEvents(client, cursor, eventRepository, settingRepository)
+		})
+	)
+}
 
-// 		C.fromIterable,
-// 		C.map(event =>
-// 			eventRepository.save({
-// 				digest: event.id.txDigest,
-// 				type: event.type,
-// 				seq: event.id.eventSeq,
-// 				payload: event.parsedJson!
-// 			})
-// 		),
-// 		E.all,
-// 		E.tap(() => {
-// 			console.log(events.nextCursor)
-// 			return settingRepository
-// 				.saveLastestCursor(events.nextCursor!)
-// 				.pipe(E.when(() => !!events.nextCursor && events.data.length > 0))
-// 		}),
-// 		// E.tap(() => E.sleep(Duration.seconds(10))),
-// 		E.as(events.nextCursor),
-// 		E.map(O.fromNullable)
-// 	)
-// }
+function handleEvents(
+	client: Web3Client,
+	cursor: EventCursor,
+	eventRepository: EventRepository,
+	settingRepository: SettingRepository
+) {
+	return pipe(
+		client.queryEvents({
+			query: {
+				MoveEventModule: { module: MODULE, package: PACKAGE }
+			},
+			order: "ascending",
+			cursor,
+			limit: 10
+		}),
+		E.tap(({ data }) =>
+			pipe(
+				data,
+				C.fromIterable,
+				C.map(event =>
+					eventRepository.save({
+						digest: event.id.txDigest,
+						type: event.type,
+						seq: event.id.eventSeq,
+						payload: event.parsedJson!,
+						timestamp: event.timestampMs
+					})
+				),
+				E.all
+			)
+		),
+		E.tap(() =>
+			console.log(`scanned to ${cursor.txDigest}::${cursor.eventSeq}`)
+		),
+		E.tap(({ data, nextCursor }) =>
+			settingRepository
+				.saveLastestCursor(nextCursor!)
+				.pipe(E.when(() => !!nextCursor && data.length > 0))
+		),
+		E.map(({ hasNextPage, nextCursor }) =>
+			pipe(
+				hasNextPage,
+				B.match({
+					onTrue: () => pipe(nextCursor, O.fromNullable),
+					onFalse: O.none
+				})
+			)
+		)
+	)
+}
 
 function getDefaultCursor(
 	client: Web3Client
-): Result<Cursor, SuiClientException | NoSuchElementException> {
+): Result<EventCursor, SuiClientException | NoSuchElementException> {
 	return pipe(
 		client.queryEvents({
 			limit: 1,
--- src/database/repositories/setting.repository.ts
+++ src/database/repositories/setting.repository.ts
@@ -2,19 +2,18 @@ import type { EventId } from "@mysten/sui/client"
 import { DatabaseException } from "@root/exceptions/database.ex.js"
 import type { Result } from "@root/types/result.type.js"
 import { eq } from "drizzle-orm"
-import { Effect as E, flow, Option as O } from "effect"
-import type { NoSuchElementException } from "effect/Cause"
+import { Effect as E, Option as O, flow, pipe } from "effect"
 import type { Db } from "../db.js"
 import { setting } from "../models/setting.model.js"
 
 export type Setting = "lastest_event_seq" | "lastest_event_tx_didest"
 
+export type EventCursor = EventId
+
 export class SettingRepository {
 	constructor(private db: Db) {}
 
-	get(
-		key: Setting
-	): Result<O.Option<string>, DatabaseException | NoSuchElementException> {
+	get(key: Setting): Result<O.Option<string>, DatabaseException> {
 		return E.tryPromise({
 			try: () =>
 				this.db.query.setting.findFirst({
@@ -48,12 +47,40 @@ export class SettingRepository {
 		)
 	}
 
-	saveLastestCursor({ eventSeq, txDigest }: EventId) {
-		return this.set("lastest_event_seq", eventSeq).pipe(
-			E.zip(this.set("lastest_event_tx_didest", txDigest), {
-				concurrent: true
+	saveLastestCursor({
+		eventSeq,
+		txDigest
+	}: EventId): Result<void, DatabaseException> {
+		return pipe(
+			E.tryPromise({
+				try: () =>
+					this.db.transaction(async tx => {
+						await tx
+							.update(setting)
+							.set({ key: "lastest_event_seq", value: eventSeq })
+							.where(eq(setting.key, "lastest_event_seq"))
+
+						await tx
+							.update(setting)
+							.set({ key: "lastest_event_tx_didest", value: txDigest })
+							.where(eq(setting.key, "lastest_event_tx_didest"))
+					}),
+				catch: error => new DatabaseException({ error })
 			}),
 			E.asVoid
+		)
+	}
+
+	getLastestCursor(): Result<O.Option<EventCursor>, DatabaseException> {
+		return pipe(
+			this.get("lastest_event_seq"),
+			E.zip(this.get("lastest_event_tx_didest")),
+			E.map(([eventSeq, txDigest]) =>
+				O.zipWith(eventSeq, txDigest, (eventSeq, txDigest) => ({
+					eventSeq,
+					txDigest
+				}))
+			)
 		)
 	}
 }
