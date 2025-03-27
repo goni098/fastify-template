528a78384af9043bc11dceb2ac036606adc45c43
goni098 <mtheanh2910@gmail.com>
2025-03-27 11:44:48 +0700

    esnest

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,74 @@ import type { Web3Client } from "./services/web3-client"
 import type { Result } from "./types/result.type"
 import { canIntoResponse, isFastifyError, traceError } from "./utils/error.util"
 
+const errorHandler = (
+	error: unknown,
+	request: FastifyRequest,
+	reply: FastifyReply
+) =>
+	M.value(error).pipe(
+		M.when(isFastifyError, err => reply.send(err)),
+		M.when(isNoSuchElementException, ex => reply.internalServerError(ex._tag)),
+		M.when(canIntoResponse, ex =>
+			pipe(
+				ex.intoResponse(),
+				response => traceError(request, response, error),
+				response => reply.status(response.code).send(response)
+			)
+		),
+		M.orElse(err => {
+			console.error("untagged error: ", err)
+			return reply.internalServerError()
+		})
+	)
+
+function main() {
+	fastify()
+		.setValidatorCompiler(validatorCompiler)
+		.setSerializerCompiler(serializerCompiler)
+		.setErrorHandler(errorHandler)
+		.register(cors)
+		.register(fastifySensible)
+		.register(fastifySwagger, {
+			openapi: {
+				info: { title: "FEFT", version: "1.0.0" },
+				components: {
+					securitySchemes: {
+						bearerAuth: {
+							type: "http",
+							scheme: "bearer",
+							bearerFormat: "JWT"
+						}
+					}
+				}
+			},
+			transform: jsonSchemaTransform
+		})
+		.register(fastifySwaggerUi, { routePrefix: "/docs" })
+		.register(dbPlugin)
+		.register(jwtPlugin)
+		.register(redisPlugin)
+		.register(unwrapResultPlugin)
+		.register(web3Plugin)
+		.register(authRouter, { prefix: "auth" })
+		.register(usersRouter, { prefix: "users" })
+		.listen({ port: 9098 }, (err, address) =>
+			pipe(
+				err,
+				O.fromNullable,
+				O.match({
+					onNone: () => console.log(`🦀 server is listening at ${address}`),
+					onSome: error => {
+						console.error(error)
+						process.exit(1)
+					}
+				})
+			)
+		)
+}
+
+main()
+
 declare module "fastify" {
 	interface FastifyInstance {
 		repositories: {
@@ -52,75 +120,3 @@ declare module "fastify" {
 		unwrapResult: <A, E>(result: Result<A, E>) => Promise<A>
 	}
 }
-
-const errorHandler = (
-	error: unknown,
-	request: FastifyRequest,
-	reply: FastifyReply
-) =>
-	M.value(error).pipe(
-		M.when(isFastifyError, err => reply.send(err)),
-		M.when(isNoSuchElementException, ex => reply.internalServerError(ex._tag)),
-		M.when(canIntoResponse, ex =>
-			pipe(
-				ex.intoResponse(),
-				response => traceError(request, response, error),
-				response => reply.status(response.code).send(response)
-			)
-		),
-		M.orElse(err => {
-			console.error("untagged error: ", err)
-			return reply.internalServerError()
-		})
-	)
-
-const server = () =>
-	fastify()
-		.setValidatorCompiler(validatorCompiler)
-		.setSerializerCompiler(serializerCompiler)
-		.setErrorHandler(errorHandler)
-		.register(cors)
-		.register(fastifySensible)
-		.register(fastifySwagger, {
-			openapi: {
-				info: { title: "FEFT", version: "1.0.0" },
-				components: {
-					securitySchemes: {
-						bearerAuth: {
-							type: "http",
-							scheme: "bearer",
-							bearerFormat: "JWT"
-						}
-					}
-				}
-			},
-			transform: jsonSchemaTransform
-		})
-		.register(fastifySwaggerUi, {
-			routePrefix: "/docs"
-		})
-		.register(dbPlugin)
-		.register(jwtPlugin)
-		.register(redisPlugin)
-		.register(unwrapResultPlugin)
-		.register(web3Plugin)
-		.register(authRouter, { prefix: "auth" })
-		.register(usersRouter, { prefix: "users" })
-
-function main(): void {
-	server().listen({ port: 9098 }, (err, address) =>
-		pipe(
-			err,
-			O.fromNullable,
-			O.match({
-				onNone: () => console.log(`🦀 server is listening at ${address}`),
-				onSome: error => {
-					console.error(error)
-					process.exit(1)
-				}
-			})
-		)
-	)
-}
-
-main()
